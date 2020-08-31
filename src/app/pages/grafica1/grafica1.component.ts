04e25e7c06e9b3d7a35b1b951565a157edba4cc7
gaabicarp <gaabicarp@gmail.com>
2020-08-31 20:29:45 -0300

    Fin seccion 6

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-grafica1',
@@ -6,11 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component {
+  
+    label1: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales']
+    label2: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales']
+    label3: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales']
+    label4: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales']
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    data1 = [
+      [350, 450, 100],
+    ];
+    data2 = [
+      [350, 450, 100],
+    ];
+    data3 = [
+      [350, 450, 100],
+    ];
+    data4 = [
+      [350, 450, 100],
+    ];
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {FormsModule} from '@angular/forms'
 import { RouterModule} from '@angular/router'
+
 
 import { PagesComponent } from './pages.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { SharedModule } from './../shared/shared.module'
-
+import { SharedModule } from './../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   declarations: [
@@ -25,8 +26,10 @@ import { SharedModule } from './../shared/shared.module'
   ],
   imports: [ 
     CommonModule,
+    FormsModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    ComponentsModule,
   ]
 })
 export class PagesModule { }
